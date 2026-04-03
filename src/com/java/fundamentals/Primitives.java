1ffa2ff75f7bf0ec1b321c1b78787695b9f5467a
vignesh9652 <panchavativignesh@gmail.com>
2026-04-03 15:26:54 +0530

    feat: new class studentName is added

---
+++ src/com/java/fundamentals/Primitives.java
@@ -0,0 +1,65 @@
+package com.java.fundamentals;
+
+public class Primitives {
+    public static void main(String[] args) {
+
+        //Byte DataType
+        byte age;
+        byte subject1Marks, subject2Marks, subject3Marks, subject4Marks, subject5Marks;
+        age =  100;
+        subject1Marks = 78;
+        subject2Marks = 98;
+        subject3Marks = 67;
+        subject4Marks = 87;
+        subject5Marks = 60;
+        System.out.println("subject1Marks: "+ subject1Marks);
+        System.out.println("subject2Marks:"+ subject2Marks);
+        System.out.println("subject3Marks:"+ subject3Marks);
+        System.out.println("subject4Marks:"+ subject4Marks);
+        System.out.println("subject5Marks:"+ subject5Marks);
+        System.out.println("subject 1 Marks: " + subject1Marks + ", subject 2 Marks: " + subject2Marks + ", subject 3 Marks: " + subject3Marks + ", subject 4 Marks: " + subject4Marks + ", subject 5 Marks: " + subject5Marks);
+        System.out.println("age: "+ age);
+
+        //Short DataType
+        short totalMarks = 190;
+        System.out.println("Total Marks:"+ totalMarks);
+        short manufacturingYear = 2022;
+        short daysInExpiry = 107;
+        System.out.println("Manufacturing Year: " + manufacturingYear + " , Days in Expiry : "+ daysInExpiry);
+
+        //Integer DataType
+        int salary = 30000;
+        int otp = 187432;
+        int price = 583989;
+        System.out.println("Salary : " + salary);
+        System.out.println("OTP : " + otp);
+        System.out.println("Price : " + price);
+
+        //Long DataType
+        long mobileNumber = 848892002L;
+        long aadharNumber = 270872763750L;
+        System.out.println("Mobile Number : "+ mobileNumber);
+        System.out.println("Aadhar Number : "+ aadharNumber);
+        System.out.println("------------------------------------------------------------------");
+        //Float DataType
+        float percentage = 45.2F;
+        float pi = 3.14F;
+        System.out.println("Percentage : "+ percentage);
+        System.out.println("Pi Value : "+ pi);
+
+        //Double DataType
+        double distance = 559.0;
+        System.out.println("Distance : "+ distance);
+
+
+        //CHAR DataType
+        char gender = 'M';
+        System.out.println("Gender : "+ gender);
+
+        //Boolean DataType
+        boolean isAvailable = true;
+        System.out.println("Is Available : "+ isAvailable);
+
+    }
+}
+
