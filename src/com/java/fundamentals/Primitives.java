eb1899a6411e713d2eb8a4ee7f6cd3d28d4bd665
vignesh9652 <panchavativignesh@gmail.com>
2026-04-03 15:30:49 +0530

    feat: Primitive DataTypes

--- src/com/java/fundamentals/Primitives.java
+++ src/com/java/fundamentals/Primitives.java
@@ -19,14 +19,14 @@ public class Primitives {
         System.out.println("subject5Marks:"+ subject5Marks);
         System.out.println("subject 1 Marks: " + subject1Marks + ", subject 2 Marks: " + subject2Marks + ", subject 3 Marks: " + subject3Marks + ", subject 4 Marks: " + subject4Marks + ", subject 5 Marks: " + subject5Marks);
         System.out.println("age: "+ age);
-
+        System.out.println("--------------------------------------------------");
         //Short DataType
         short totalMarks = 190;
         System.out.println("Total Marks:"+ totalMarks);
         short manufacturingYear = 2022;
         short daysInExpiry = 107;
         System.out.println("Manufacturing Year: " + manufacturingYear + " , Days in Expiry : "+ daysInExpiry);
-
+        System.out.println("-------------------------------------------------");
         //Integer DataType
         int salary = 30000;
         int otp = 187432;
@@ -34,6 +34,7 @@ public class Primitives {
         System.out.println("Salary : " + salary);
         System.out.println("OTP : " + otp);
         System.out.println("Price : " + price);
+        System.out.println("-----------------------------------------------------");
 
         //Long DataType
         long mobileNumber = 848892002L;
@@ -46,16 +47,16 @@ public class Primitives {
         float pi = 3.14F;
         System.out.println("Percentage : "+ percentage);
         System.out.println("Pi Value : "+ pi);
-
+        System.out.println("--------------------------------------------------------------");
         //Double DataType
         double distance = 559.0;
         System.out.println("Distance : "+ distance);
-
+        System.out.println("-----------------------------------------------------------");
 
         //CHAR DataType
         char gender = 'M';
         System.out.println("Gender : "+ gender);
-
+        System.out.println("-----------------------------------------------------------");
         //Boolean DataType
         boolean isAvailable = true;
         System.out.println("Is Available : "+ isAvailable);
